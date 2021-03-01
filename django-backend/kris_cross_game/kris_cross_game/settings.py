7416ddf71f1a2cc81e55c5311bcc4d3db6c6c907
petia2009 <familia19842009@gmail.com>
2021-03-01 20:00:29 +0300

    django server 3 game.html design end

--- django-backend/kris_cross_game/kris_cross_game/settings.py
+++ django-backend/kris_cross_game/kris_cross_game/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.6']
+ALLOWED_HOSTS = ['192.168.1.6', '127.0.0.1']
 
 
 # Application definition
