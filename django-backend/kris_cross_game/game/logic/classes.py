c76c66b7c8a3f9b5e6af4dc36653ce359190a11d
petia2009 <familia19842009@gmail.com>
2021-03-24 22:31:04 +0300

    django server 6 field

--- django-backend/kris_cross_game/game/logic/classes.py
+++ django-backend/kris_cross_game/game/logic/classes.py
@@ -12,13 +12,53 @@ class Game:
 class FieldXO:
     def __init__(self):
         self.__field__ = [
-            ['X', ' ', ' '],
-            ['X', ' ', ' '],
-            ['X', ' ', ' ']
+            [' ', ' ', ' '],
+            [' ', ' ', ' '],
+            [' ', ' ', ' ']
         ]
+        self.move_x = True
+        self.move_y = False
 
     def move(self, x_or_o, x, y):
         self.__field__[y][x] = x_or_o
+        self.move_x, self.move_y = self.move_y, self.move_x
+
+    def get_whoose_move(self):
+        if self.move_x:
+            return "X"
+        else:
+            return "O"
 
     def get_field(self):
         return self.__field__
+
+    def is_winner(self, player_type):
+        for i in range(3):
+            has_winner = False
+            if self.__field__[i][0] != player_type:
+                continue
+            for j in range(1, 3):
+                if self.__field__[i][j] != player_type:
+                    has_winner = False
+                    break
+                else:
+                    has_winner = True
+            if has_winner:
+                return True
+
+        for i in range(3):
+            has_winner = False
+            if self.__field__[0][i] != player_type:
+                continue
+            for j in range(1, 3):
+                if self.__field__[j][i] != player_type:
+                    has_winner = False
+                    break
+                else:
+                    has_winner = True
+            if has_winner:
+                return True
+
+        if (self.__field__[0][0] == player_type and self.__field__[2][2] == player_type) \
+                or (self.__field__[0][2] == player_type and self.__field__[2][0] == player_type):
+            return True
--- django-backend/kris_cross_game/game/views.py
+++ django-backend/kris_cross_game/game/views.py
@@ -3,13 +3,18 @@ from .models import Players
 from django.http import JsonResponse
 from .logic import classes
 
+fieldXO = classes.FieldXO()
 
 def game(req):
-    fieldXO = classes.FieldXO()
+    whoose_move = fieldXO.get_whoose_move()
     game_id = req.COOKIES.get('game_id')
     players = Players.objects.order_by("name")
     player = req.POST.get('username')
     coords = req.POST.get("coords")
+    if not coords == None:
+        coords = coords.split(" ")
+        coords = list(map(lambda a: int(a), coords))
+        fieldXO.move(whoose_move, coords[1], coords[0])
     context = {
         "list_players": players,
         "player1": player,
